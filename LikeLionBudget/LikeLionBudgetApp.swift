2efe3d4f3f3a815bf6e1937fe76f05759e486d62
Samuel Kim <sammy03312@gmail.com>
2026-01-13 19:26:24 -0800

    Initial Commit

---
+++ LikeLionBudget/LikeLionBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  LikeLionBudgetApp.swift
+//  LikeLionBudget
+//
+//  Created by samuel kim on 1/13/26.
+//
+
+import SwiftUI
+
+@main
+struct LikeLionBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
